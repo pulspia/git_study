17ff26bb3fbb86d1a85b0e6b578a585a798dad6d
pulspia <pulspia@gmail.com>
2019-07-08 17:45:06 +0900

    atom git commit test

--- my_module.py
+++ my_module.py
@@ -1,15 +1,18 @@
 def add(a,b):
     return a+b
-	
+
 def sub(a,b):
     return a-b
-	
+
 def mul(a,b):
     return a*b
-	
+
 def div(a,b):
     return a/b
 
 def div2(a,b):
     return a//b # 주석2
-	
+
+def div3(a,b):
+    return a%b
+        
